6bc20ac787f9f74f7fc576bebcca504b70c4c776
renual27 <rafal.buchner@gmail.com>
2019-06-10 18:15:54 +0200

    init 3

---
+++ GlyphPalette.roboFontExt/lib/glyphsRepresentation.py
@@ -0,0 +1,131 @@
+from fontTools.pens.pointInsidePen import PointInsidePen
+from mojo.drawingTools import *
+import math
+
+class GlyphRepr:
+
+    def __init__(self, glyph, origin, fillColor=(0,0,0,1), shift=None,offset=0):
+        self.bcColor = 0, 0, 1, .3
+        self.glyph = glyph
+        self.name = glyph.name
+        self.fill = fillColor
+        self.origin = origin
+        if shift is not None:
+            self.shift = shift
+        else:
+            self.shift = -glyph.width/2
+        self.rotation = 0
+        self.offset = offset
+        self.areaRect = (0, self.glyph.font.info.descender, self.glyph.width, self.glyph.font.info.ascender-self.glyph.font.info.descender)
+        self.scale = 1
+
+    def isInside(self, cursor):
+        pen = PointInsidePen(None, cursor)
+        for i, point in enumerate(self.activeAreaRect):
+            if i == len(self.activeAreaRect) - 1:
+                pen.lineTo(point)
+                pen.closePath()
+                break
+            elif i == 0:
+                pen.moveTo(point)
+            else:
+                pen.lineTo(point)
+        result = pen.getResult()
+        if result:
+            self.bcColor = 0, 0, 1, .3
+        else: self.bcColor = 1, 0, 0, .3
+        return result
+
+
+
+    def draw(self):
+        save()
+        fill(*self.fill)
+        scale(self.scale)
+        translate(self.origin[0]/self.scale, self.origin[1]/self.scale)
+        rotate(math.degrees(self.rotation))
+        translate(self.shift,0)
+        translate(0, self.offset/self.scale)
+        drawGlyph(self.glyph)
+        #
+        # fill(*self.bcColor)
+        # rect(*self.areaRect)
+        #
+        restore()
+
+    @property
+    def activeAreaRect(self):
+        activeAreaRect = []
+        x,y,w,h = self.areaRect
+        for point in [
+                (x, y),
+                (x+w, y),
+                (x+w, y+h),
+                (x,   y+h)
+            ]:
+
+            point = (point[0]*self.scale, point[1]*self.scale)
+            point = GlyphRepr.movePointTo(point,(self.origin[0],self.origin[1]))
+            point = GlyphRepr.movePointTo(point,(self.shift*self.scale,0))
+            point = GlyphRepr.movePointTo(point,(0, self.offset))
+            point = GlyphRepr.rotatePoint(point, self.rotation, (self.origin[0],self.origin[1]))
+            activeAreaRect += [point]
+        return activeAreaRect
+
+    @property
+    def rotation(self):
+        return self._rotation
+
+    @rotation.setter
+    def rotation(self, radians):
+        self._rotation = radians
+
+    @staticmethod
+    def movePointTo(p, offset) -> tuple:
+        x,y = p
+        ox, oy = offset
+        return (x+ox, y+oy)
+
+    @staticmethod
+    def angle(p1, p2):
+        p1x, p1y = p1
+        p2x, p2y = p2
+        return math.atan2(p2x - p1x, p2y - p1y)
+
+    @staticmethod
+    def rotatePoint(P, radians, originPoint):
+        alfa = radians
+        px,py=P
+        originPointX, originPointY = originPoint
+
+        x = ( px - originPointX ) * math.cos( alfa ) - ( py - originPointY ) * math.sin( alfa ) + originPointX
+        y = ( px - originPointX ) * math.sin( alfa ) + ( py - originPointY ) * math.cos( alfa ) + originPointY
+
+        return x, y
+if __name__ == "__main__":
+    def dp(p):
+        x,y = p
+        s = 10
+        r = s/2
+        rect(x-r,y-r,s,s)
+
+    f  = CurrentFont()
+    g1 = f['W']
+
+    gr = GlyphRepr(g1,origin=(-30, 70),fillColor=(1,0,0,.8),shift=None,offset=220)
+    translate(500, 200)
+    scale(.8)
+    gr.scale = .2
+    gr.rotation = radians(13)
+
+    save()
+    fill(None)
+    stroke(0,0,1,.3)
+    strokeWidth(7)
+    line((-1000,0),(1000,0))
+    line((0,-1000),(0,1000))
+
+    cursor = (-30, 430)
+    dp(cursor)
+    print(gr.isInside(cursor))
+    gr.draw()
---
+++ GlyphPalette.roboFontExt/lib/showGlyphsWithCurrentAsComponent.py
@@ -0,0 +1,235 @@
+import AppKit
+from mojo.events import addObserver, removeObserver, EditingTool
+import vanilla, math
+from mojo.extensions import setExtensionDefault, getExtensionDefault
+from glyphsRepresentation import GlyphRepr
+from mojo.drawingTools import *
+from mojo.UI import UpdateCurrentGlyphView, CurrentGlyphWindow, OpenSpaceCenter, getDefault
+
+def isNumEven(num):
+    if (num % 2) == 0:
+       return True
+    else:
+       return False
+class SettingsGet:
+    def __init__(self, value):
+        self.value = value
+    def get(self):
+        if self.value:
+            return 1
+        else: return 0
+class ShowGlyphPalette:
+    bcColor = AppKit.NSColor.grayColor()
+    fill = getDefault("glyphViewFillColor")
+    attr = {
+                AppKit.NSFontAttributeName: AppKit.NSFont.fontWithName_size_("Monaco", 8.0),
+                AppKit.NSForegroundColorAttributeName: AppKit.NSColor.whiteColor()
+            }
+    key = "com.rafalbuchner.ShowGlyphPalette"
+
+    def __init__(self):
+        # self.debugW = vanilla.FloatingWindow((0,0,100,100))
+        # self.debugW.bind("close",self.closingDebug)
+        # self.debugW.open()
+        self.isCursorAbove = False
+        self.glyphList = []
+        self.methodsToDraw = []
+        self.methodsToDrawBackground = []
+        self.itemsCallbacks = {'show related cluster': self.showGlyphsWithCurrentCB, 'show related in back': self.showRelatedInBackCB}
+        self.items = {'show related cluster': True, 'show related in back': True}
+        self.loadSettings()
+        addObserver(self, "glyphAdditionContextualMenuItemsCB", "glyphAdditionContextualMenuItems")
+        addObserver(self, "currentGlyphChangedCB", "currentGlyphChanged")
+        addObserver(self, "drawCB", "draw")
+        addObserver(self, "drawBackgroundCB", "drawBackground")
+        addObserver(self, "mouseMovedCB", "mouseMoved")
+        addObserver(self, "mouseDownCB", "mouseDown")
+
+    # def closingDebug(self, info):
+    #     removeObserver(self, "glyphAdditionContextualMenuItems")
+    #     removeObserver(self, "currentGlyphChanged")
+    #     removeObserver(self, "draw")
+    #     removeObserver(self, "drawBackground")
+    #     removeObserver(self, "mouseMoved")
+    #     removeObserver(self, "mouseDown")
+
+    def drawRelatedGlyphWindow(self, cursor, glyph, view):
+        view._drawTextInRect(f"name:  {glyph.name}\nwidth: {glyph.width}",
+        self.attr, cursor, yOffset=10, xOffset=10, drawBackground=True, position="left", backgroundColor=self.bcColor)
+
+    def mouseDownCB(self, point):
+        if point['clickCount'] == 2:
+            if self.isCursorAbove:
+                CurrentGlyphWindow().setGlyphByName(self.glyphBelowName)
+
+    def mouseMovedCB(self, info):
+
+        self.cursor = (info['point'].x, info['point'].y)
+        self.isCursorAbove = False
+        for glyphRepr in self.glyphList:
+            if glyphRepr.isInside(self.cursor):
+                self.view = info["view"]
+                self.glyphBelowName = glyphRepr.name
+                self.isCursorAbove = True
+
+                break
+        UpdateCurrentGlyphView()
+
+    def currentGlyphChangedCB(self, sender):
+        self.glyph = CurrentGlyph()
+        if self.glyph is None:
+            return
+        font = self.glyph.font
+        self.glyphList = []
+        for glyph in font:
+            try:
+                for component in glyph.components:
+                    if component.baseGlyph == self.glyph.name:
+                        glyphRepr = GlyphRepr(font[glyph.name],origin=(self.glyph.width/2, self.glyph.bounds[-1]/2),fillColor=self.fill,shift=None,offset=font.info.ascender*1)
+
+                        self.glyphList += [glyphRepr]
+
+            except:
+                print(f"glyph <{glyph.name}> contains corrupted component")
+        if len(self.glyphList) > 0:
+            scale = 3/len(self.glyphList)
+            if scale > .4:
+                scale = .4
+            for glyph in self.glyphList:
+                glyph.scale = scale
+
+    @staticmethod
+    def appendMethodToDrawingMethods(sender, method,drawingMethods):
+        if sender.get() == 1:
+            if method not in drawingMethods:
+                drawingMethods.append(method)
+                UpdateCurrentGlyphView()
+        else:
+            if method in drawingMethods:
+                drawingMethods.remove(method)
+                UpdateCurrentGlyphView()
+
+    def showRelatedInBackCB(self, sender):
+        ShowGlyphPalette.appendMethodToDrawingMethods(sender, self.showRelatedInBackDraw, self.methodsToDrawBackground)
+
+    def showGlyphsWithCurrentCB(self, sender):
+        ShowGlyphPalette.appendMethodToDrawingMethods(sender, self.showGlyphsWithCurrentDraw, self.methodsToDrawBackground)
+
+    def showRelatedInBackDraw(self, scale):
+        for glyphRepr in self.glyphList:
+            glyph = self.glyph.font[glyphRepr.name]
+
+            shift = (self.glyph.width - glyph.width)/2
+            glyphReprBack = GlyphRepr(
+                glyph,
+                origin=(0, 0),
+                fillColor = self.fill,
+                shift=shift,
+                offset=0)
+            stroke(None)
+            glyphReprBack.draw()
+
+    def drawPreviewCB(self, scale):
+        pass
+
+    def showGlyphsWithCurrentDraw(self, scale):
+        def _getGlyphWidth(glyphRepr):
+            return glyphRepr.glyph.width
+        if self.glyphList:
+            widestGlyph = max(self.glyphList, key=_getGlyphWidth)
+            ox,oy = widestGlyph.origin
+            rightSB = (ox+widestGlyph.glyph.width*widestGlyph.scale+10, oy+widestGlyph.offset)
+            rotationAngle = -GlyphRepr.angle((ox, oy),rightSB)
+            if not isNumEven(len(self.glyphList)):
+                offsetAngle = (round(len(self.glyphList)/2)-1)* rotationAngle
+            else:
+                offsetAngle = (len(self.glyphList)/2-.5)* rotationAngle
+
+            angle = 0
+            for i, glyphRepr in enumerate(self.glyphList):
+                glyphRepr.rotation = angle - offsetAngle
+                angle = (i+1)*rotationAngle
+                stroke(None)
+
+                glyphRepr.draw()
+
+    def drawCB(self, scale):
+        if self.isCursorAbove:
+            glyph = self.glyph.font[self.glyphBelowName]
+            self.drawRelatedGlyphWindow(self.cursor, glyph, self.view)
+
+        for method in self.methodsToDraw:
+            method(scale)
+
+    def drawBackgroundCB(self, scale):
+        for method in self.methodsToDrawBackground:
+            method(scale)
+
+    def loadSettings(self):
+        # loading settings
+        zero_settings = self.items
+        settings = getExtensionDefault(self.key, fallback=self.items)
+        if settings:
+            for name in settings:
+                self.items[name] = settings[name]
+            else:
+                self.items = zero_settings
+        else:
+            self.items = zero_settings
+
+    def savingSettings(self):
+        setExtensionDefault(self.key, self.items)
+
+
+    def glyphAdditionContextualMenuItemsCB(self, info):
+        menuItems = info['additionContextualMenuItems']
+        showGlyphPaletteMenuItems = []
+        tool = info["tool"]
+        if isinstance(tool, EditingTool):
+
+            for title in self.items:
+                temp = title.replace(" ","_")
+                value = self.items[title]
+                checkboxMenuItem = AppKit.NSMenuItem.alloc().initWithTitle_action_keyEquivalent_("regular", '', '')
+                MenuTextAttributes = {AppKit.NSFontAttributeName: AppKit.NSFont.menuFontOfSize_(14)}
+                t =  AppKit.NSAttributedString.alloc().initWithString_attributes_(title, MenuTextAttributes)
+                checkbox = vanilla.CheckBox((0, 0, 100, 22), t, value=value, callback=self.checkboxCallback)
+                setattr(self, temp, checkbox)
+                view =checkbox.getNSButton()
+                view.setFrame_(((0, 0), (200, 30)))
+                checkboxMenuItem.setView_(view)
+                showGlyphPaletteMenuItems += [checkboxMenuItem]
+                cb = self.itemsCallbacks.get(title)
+                if cb is not None:
+                    cb(checkbox)
+
+            showGlyphPaletteMenuItems += [
+                ("print out palette", self.printOutClusterCallback),
+                ("open palette in SpaceCenter", self.openClusterInSpaceCenterCallback),
+                ("select palette in font", self.selectClusterCallback)
+            ]
+        menuItems += [("Glyph Palette", showGlyphPaletteMenuItems)]
+    def selectClusterCallback(self, sender):
+        self.glyph.font.selectedGlyphNames = [glyph.glyph.name for glyph in self.glyphList]
+
+
+
+    def openClusterInSpaceCenterCallback(self, sender):
+        spaceCenter = OpenSpaceCenter(self.glyph.font)
+        spaceCenter.setPre([glyph.glyph.name for glyph in self.glyphList])
+
+
+    def printOutClusterCallback(self, sender):
+        print([glyph.glyph.name for glyph in self.glyphList])
+
+    def checkboxCallback(self, obj):
+        value = False
+        if obj.get() == 1:
+            value = True
+        self.items[obj.getTitle()] = value
+        self.savingSettings()
+
+        self.itemsCallbacks[obj.getTitle()](obj)
+
+
+ShowGlyphPalette()
